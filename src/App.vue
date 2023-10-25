98ff442e04011428f18630589512d9fd1b8550db
LeoPavlovski <lp29379@seeu.edu.mk>
2023-10-25 19:20:02 +0200

    First commit, starting with the quiz project.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+    <app-bar></app-bar>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import AppBar from "@/components/AppBar.vue";
+
+export default {
+  components: {AppBar},
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,6 @@
+<template>
+<!--  Ok, so we are going to have 1 appbar with a button to take the quiz.-->
+  <div>
+    <p>this is the appbar</p>
+  </div>
+</template>
